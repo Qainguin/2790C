59f343ec297115dadf2cec894bfbfbe12097ee53
Qainguin <94211064+Qainguin@users.noreply.github.com>
2024-03-19 12:34:11 -0500

    changed

--- drivetrain6m/src/main.cpp
+++ drivetrain6m/src/main.cpp
@@ -33,16 +33,6 @@ motor_group all_drive_motors = motor_group(l1,l2,l3,r1,r2,r3);
 motor_group left_drive_motors = motor_group(l1,l2,l3);
 motor_group right_drive_motors = motor_group(r1,r2,r3);
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
-
 // A function that moves the drivetrain forwards and backwards by the x_strength parameter.
 // It also turns left and right by the y_strength parameter.
 void move_drivetrain(float x_strength, float y_strength) {
@@ -57,50 +47,17 @@ void move_drivetrain(float x_strength, float y_strength) {
 }
 
 void pre_auton(void) {
-
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
+  
   all_drive_motors.setStopping(brake);
-
-  Brain.Screen.setPenColor(white);
-
-  Brain.Screen.setFillColor(white);
-  Brain.Screen.drawRectangle(0,0,480,272);
-
-  Brain.Screen.setFillColor(green);
-  Brain.Screen.drawRectangle(10,10,460,222);
-
-  Brain.Screen.setPenColor(black);
-  Brain.Screen.setFont(mono20);
-
-  Brain.Screen.printAt(Brain.Screen.getStringWidth("This is centered"), 136, "This is centered");
 }
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
 }
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
   // User control code here, inside the loop
